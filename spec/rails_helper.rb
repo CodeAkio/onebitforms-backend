40c586a59331061c1c6f02c9780de0d7b999197f
CodeAkio <siqueira_2004@hotmail.com>
2021-03-11 17:28:07 -0300

    Fix rails_helper config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,14 +9,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-# Custom json helpers
-config.include Requests::JsonHelpers, type: :request
-# Custom Header helpers
-config.include Requests::HeaderHelpers, type: :request
-
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
-config.include Devise::Test::ControllerHelpers, type: :controller
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -42,6 +35,13 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # Custom json helpers
+  config.include Requests::JsonHelpers, type: :request
+  # Custom Header helpers
+  config.include Requests::HeaderHelpers, type: :request
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
